81ca203a871edb07d0257ce7cf40315ffdd67d3d
zonixx-it <goodcrazyson@gmail.com>
2025-10-04 20:05:32 +0500

    feat: add new functionality to Maze class
    - Add fundamental parameters to Maze class (data, rowsCount, colsCount)
    - Make parameters private and restrict direct access
    - Add getter functions for Maze class parameters

--- src/maze_solver/core/models.py
+++ src/maze_solver/core/models.py
@@ -1,6 +1,20 @@
-class Maze:
-    pass
-
-
 class Cell:
     pass
+
+
+class Maze:
+    def __init__(self, rowsCount: int, colsCount: int):
+        self.__rowsCount: int = rowsCount
+        self.__colsCount: int = colsCount
+        self.__data: list[list[Cell]] = [
+            [Cell() for _ in range(colsCount)] for _ in range(rowsCount)
+        ]
+
+    def get_rows_count(self) -> int:
+        return self.__rowsCount
+
+    def get_cols_count(self) -> int:
+        return self.__colsCount
+
+    def get_cell(self, rowIndex, colIndex) -> Cell:
+        return self.__data[rowIndex][colIndex]
